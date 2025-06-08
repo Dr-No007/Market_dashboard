a33d7c99bcf5ec801776d670d00a69474a372ae9
Dr-No007 <calumbalish@protonmail.com>
2025-06-08 14:03:33 +0100

    Add files via upload
    2nd attempt using lab based learning

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,10 +1,41 @@
-print("Hello, script is running!")
+# Market dahsboard project
+# First Objectives
+# 1. Importing necessary libraries 
+# 2 Define the list of futures symbols to track
+# 3. fetch historical data for each symbol
+# 4. IDisplay data for 1 insrument
+
+#step 1 importing necessary libraries
+# Using 'yfinance' to fetch historical data
 import yfinance as yf
-print("Trying to fetch data...")
-ticker = "ES=F"
-try:
-    data = yf.Ticker(ticker).history(period="1mo")
-    print(f"Data for {ticker}:\n", data[['Open', 'High', 'Low', 'Close']].tail())
-except Exception as e:
-    print(f"Error: {e}")
-print("Done!")
+
+# using 'pandass' for data manipulation(yfinance returns data in a pandas DataFrame)
+import pandas as pd
+
+#set a pandas option to display all columns in dataframe(helpful for adding more later)
+pd.set_option('display.max_columns', None)
+
+#step 2 define instruments
+futures_tickers = ["ES=F", "NQ=F", "CL=F", "GC=F"]
+
+#step 3 fetch historical data for each symbol
+#create a dictionary to hold the data this will be used for storing data as a key
+historical_data = {}
+print("fetching data...")
+
+#create a loop to fetch data for each ticker
+for ticker in futures_tickers:
+    try:
+        futures = yf.Ticker(ticker) # create a ticker for current future
+        data = futures.history(period="2y") # fetch historical data for the last 2 years
+        data = data[['Open', 'High', 'Low', 'Close', 'Volume']]  # select only the relevant columns
+        historical_data[ticker] = data # store the data in the dictionary
+        print(f"successfully fetched data for {ticker}")
+    except Exception as e: 
+        print(f"Could not fetch data for {ticker}. Error: {e}") # if there is an error, print it
+print("\nData fetching complete.")
+
+#step 4 display data for one instrument
+if "ES=F" in historical_data:
+    print("\n--- sample data for ES=F ---")
+print(historical_data["ES=F"].tail(1))  # display the last 5 rows of data for ES=F
